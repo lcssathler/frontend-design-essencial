f31e80b906c87452675a1203197523846b1a3953
lcssathler <lcssathler@gmail.com>
2023-07-25 20:54:42 -0300

    Preloader

--- projeto-final/js/main.js
+++ projeto-final/js/main.js
@@ -1,5 +1,13 @@
 var btnContact = document.querySelector('.ls-btn-contact');
 
+window.addEventListener("load",  () => {
+    var pagePreloader = document.querySelector(".ls-preloader-inner");
+    setTimeout(() => {
+        pagePreloader.classList.add("ls-fade-out");
+    }, 5000)
+    
+})
+
 btnContact.addEventListener("click", function() {
     var boxInfo = document.querySelector(".ls-contact-info");
     boxInfo.classList.toggle("ls-is-open");
