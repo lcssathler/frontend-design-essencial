5127a7013d80fd46dc391c6b436d2c09d89c3117
lcssathler <lcssathler@gmail.com>
2023-08-08 18:55:01 -0300

    Fade out animation on scroll

--- projeto-final/js/main.js
+++ projeto-final/js/main.js
@@ -1,6 +1,7 @@
 var btnContact = document.querySelector(".ls-btn-contact");
 var toggleModal = document.querySelectorAll('.ls-toggle-modal');
 
+//Preloader
 window.addEventListener("load", () => {
 	var pagePreloader = document.querySelector(".ls-preloader-inner");
 	pagePreloader.classList.add("ls-fade-out");
@@ -9,12 +10,14 @@ window.addEventListener("load", () => {
 	}, 2000);
 });
 
+//Show box contact
 btnContact.addEventListener("click", function () {
 	var boxInfo = document.querySelector(".ls-contact-info");
 	boxInfo.classList.toggle("ls-is-open");
 	this.classList.toggle("ls-change-position-icon");
 });
 
+//Open and close modal and overlay
 for(i = 0; i < toggleModal.length; i++) {
 	toggleModal[i].addEventListener('click', () => {
 		var overlay = document.querySelector('.ls-overlay')
@@ -24,3 +27,15 @@ for(i = 0; i < toggleModal.length; i++) {
 		budgetModal.classList.toggle("ls-is-open");
 	})
 }
+
+//Animation on scroll
+var myScrollDown = document.querySelector('.ls-scroll-down');
+var waypoint = new Waypoint(
+	{
+		element: myScrollDown,
+		handler: function() {
+			myScrollDown.classList.toggle('ls-fade-out');
+  		},
+		offset: '80%'
+	}
+);
