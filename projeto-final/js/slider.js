68dcb08596705a69bf92fd568b42b490b2fc9e4c
lcssathler <lcssathler@gmail.com>
2023-08-14 19:34:15 -0300

    Slider count

--- projeto-final/js/slider.js
+++ projeto-final/js/slider.js
@@ -3,6 +3,12 @@
 var sliderContainer = document.querySelector(".ls-slider-container");
 var sliderList = document.querySelector(".ls-slider-list");
 var sliderItem = document.querySelectorAll(".ls-slider-item");
+var currentSlide = document.querySelector(".ls-current-slide");
+var totalSlide = document.querySelector(".ls-total-slide")
+var prevItem = document.querySelector(".ls-item-prev");
+var nextItem = document.querySelector(".ls-item-next");
+var indexList = sliderItem.length - 1;
+var pos = 0;
 
 var containerWidth = sliderContainer.parentElement.offsetWidth;
 var sliderListWidth = containerWidth * sliderItem.length;
@@ -15,17 +21,11 @@ for (let i = 0; i < sliderItem.length; i++) {
 
 sliderList.style.width = sliderListWidth + "px";
 
-//Slider onClick animation
-
-var prevItem = document.querySelector(".ls-item-prev");
-var nextItem = document.querySelector(".ls-item-next");
-var indexList = sliderItem.length - 1;
-var pos = 0;
 
 //HANDLERS
 
 //Next slider animation
-var nextSliderAnimation = () => {
+var nextSlideAnimation = () => {
 	if (pos <= containerWidth * indexList * -1) {
 		return;
 	}
@@ -39,7 +39,7 @@ var nextSliderAnimation = () => {
 };
 
 //Previous slider animation
-var prevSliderAnimation = () => {
+var prevSlideAnimation = () => {
 	if (pos >= 0) {
 		return;
 	}
@@ -51,10 +51,37 @@ var prevSliderAnimation = () => {
 	});
 };
 
+//Counter formatter to two digits
+var counterFormatter = n => {
+    if(n < 10) {
+        return "0" + n;
+    } else {
+        return n;
+    }
+}
+
+var addCount = () => {
+    let atualSlidePosition = -1 * (pos / containerWidth) + 1;
+    console.log(atualSlidePosition);
+    return counterFormatter(atualSlidePosition);
+}
+
+var subtractCount = () => {
+    let atualSlidePosition = -1 * (pos / containerWidth) + 1;
+    console.log(atualSlidePosition);
+    return counterFormatter(atualSlidePosition);
+}
+
+//ACTIONS
+
+totalSlide.innerHTML = counterFormatter(sliderItem.length);
+
 nextItem.addEventListener("click", () => {
-    nextSliderAnimation();
+    nextSlideAnimation();
+    currentSlide.innerHTML = addCount();
 });
 
 prevItem.addEventListener("click", () => {
-    prevSliderAnimation();
+    prevSlideAnimation();
+    currentSlide.innerHTML = subtractCount();
 });
