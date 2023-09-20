6b25bc7f0d6d972fcb9587fb779f0ab824205d78
Lucas Sathler <lcssathler@gmail.com>
2023-09-20 20:34:55 -0300

    manage it - skeleton loading in next/prev buttons

--- projeto-final/pages/portfolio/js/manage-it.js
+++ projeto-final/pages/portfolio/js/manage-it.js
@@ -1,96 +1,116 @@
 var galleryImages = document.querySelectorAll(".ls-thumb-img");
 var overlay = document.querySelector(".ls-overlay");
-var galleryFrameContainer
+var galleryFrameContainer;
 var galleryFrameInner = document.querySelector(".ls-gallery-frame-inner");
 var frameImg = document.querySelector(".ls-gallery-frame-img");
 var toggleGallery = document.querySelectorAll(".ls-toggle-gallery");
 var currentGalleryImage = null;
 var prevBtn = document.querySelector(".ls-item-prev");
 var nextBtn = document.querySelector(".ls-item-next");
-
 var currentCounter = document.querySelector(".ls-current-image");
 var totalCounter = document.querySelector(".ls-total-image");
+var skeletonLoading = document.querySelector(".ls-skeleton-loading");
 
 totalCounter.innerHTML = galleryImages.length;
-
 
 AOS.init();
 var rellax = new Rellax(".rellax");
 
 for (const element of galleryImages) {
-	new Waypoint.Inview({
-		element: element,
-		entered: function () {
-			let fullImg = this.element.getAttribute("data-src");
-			this.element.setAttribute("src", fullImg);
-		},
-	});
+  new Waypoint.Inview({
+    element: element,
+    entered: function () {
+      let fullImg = this.element.getAttribute("data-src");
+      this.element.setAttribute("src", fullImg);
+    },
+  });
 }
-
 
 //HANDLERS
 
 var counterFormatter = (n) => {
-	n++;
-	if (n < 10) {
-		return "0" + n;
-	} else {
-		return n;
-	}
-}; 
+  n++;
+  if (n < 10) {
+    return "0" + n;
+  } else {
+    return n;
+  }
+};
 
 var openOverlay = () => {
-	overlay.classList.add("ls-is-open")
-    galleryFrameInner.classList.add("ls-is-open");
+  overlay.classList.add("ls-is-open");
+  galleryFrameInner.classList.add("ls-is-open");
 };
 
 var closeOverlay = () => {
-	overlay.classList.remove("ls-is-open");
-    galleryFrameInner.classList.remove("ls-is-open");
-}
+  overlay.classList.remove("ls-is-open");
+  galleryFrameInner.classList.remove("ls-is-open");
+};
 
-var setImage = item => {
-	let galleryItemSrc = item.getAttribute("src");
-	frameImg.setAttribute("src", galleryItemSrc);
-}
+var setImage = (item) => {
+  let galleryItemSrc = item.getAttribute("src");
+  frameImg.setAttribute("src", galleryItemSrc);
+};
 
+var closeSkeleton = (galleryImage) => {
+  var myImage = new Image();
+  myImage.src = galleryImage.getAttribute("src");
+  myImage.addEventListener("load", () => {
+    skeletonLoading.classList.add("ls-fade-out");
+    setTimeout(() => {
+      console.log("Loaded!");
+      skeletonLoading.style.display = "none";
+    }, 2000);
+  });
+};
 
 for (let i = 0; i < galleryImages.length; i++) {
-	let item = galleryImages[i];
-	item.addEventListener("click", () => {
-		openOverlay();
-		currentGalleryImage = item;
-		setImage(currentGalleryImage);
-		currentCounter.innerHTML = counterFormatter(i);
-	});
+  let item = galleryImages[i];
+  item.addEventListener("click", () => {
+    openOverlay();
+    skeletonLoading.style.display = "flex";
+
+    currentGalleryImage = item;
+    setImage(currentGalleryImage);
+    currentCounter.innerHTML = counterFormatter(i);
+
+    closeSkeleton(currentGalleryImage);
+  });
 }
 
 prevBtn.addEventListener("click", () => {
-	var index = parseInt(currentGalleryImage.getAttribute("data-index"));
-	if (index >= 1) {
-		currentGalleryImage = galleryImages[index - 1];	
-		currentCounter.innerHTML = counterFormatter(index - 1);
-	} else {
-		currentGalleryImage = galleryImages[0];
-		currentCounter.innerHTML = counterFormatter(index);
-	}
-	setImage(currentGalleryImage);
-})
+  var index = parseInt(currentGalleryImage.getAttribute("data-index"));
+  if (index == 0) {
+    currentGalleryImage = galleryImages[0];
+    currentCounter.innerHTML = counterFormatter(index);
+    return;
+  }
+
+  skeletonLoading.style.display = "flex";
+  currentGalleryImage = galleryImages[index - 1];
+  currentCounter.innerHTML = counterFormatter(index - 1);
+  setImage(currentGalleryImage);
+  closeSkeleton(currentGalleryImage);
+});
 
 nextBtn.addEventListener("click", () => {
-	var index = parseInt(currentGalleryImage.getAttribute("data-index"));
-	if (index < 11) {
-		currentGalleryImage = galleryImages[index + 1];	
-		currentCounter.innerHTML = counterFormatter(index + 1);
-	} else {
-		currentGalleryImage = galleryImages[11];
-		currentCounter.innerHTML = counterFormatter(index);
-	}
-	setImage(currentGalleryImage);
-})
- 
+  var index = parseInt(currentGalleryImage.getAttribute("data-index"));
+  var lastIndex = galleryImages.length - 1;
+  if (index == lastIndex) {
+    currentGalleryImage = galleryImages[11];
+    currentCounter.innerHTML = counterFormatter(index);
+    return;
+  }
+
+  skeletonLoading.style.display = "flex";
+  currentGalleryImage = galleryImages[index + 1];
+  currentCounter.innerHTML = counterFormatter(index + 1);
+  setImage(currentGalleryImage);
+  closeSkeleton(currentGalleryImage);
+});
+
 for (let item of toggleGallery) {
-	item.addEventListener("click", () => {
-		closeOverlay();
-	});
+  item.addEventListener("click", () => {
+    closeOverlay();
+  });
 }
